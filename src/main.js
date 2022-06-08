b1050db79f1201bf4b0a06774cb93818934f9433
Uwe Sölen Flaig <uwe.flaig@hella-gutmann.com>
2022-06-08 16:51:58 +0200

    Added modal feature.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log('hello world!')
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    document.querySelector( '#new-update' ).showModal();
+  },
+  onOfflineReady() {
+    console.log('')
+  },
+})
+
+document.querySelector('#new-update-button').addEventListener( 'click', () => {
+    console.log('blick');
+})
