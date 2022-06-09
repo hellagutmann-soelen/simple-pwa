f0862d11295747a477808dfff63057edf5742723
Uwe Sölen Flaig <uwe.flaig@hella-gutmann.com>
2022-06-09 09:57:54 +0200

    Added dynamic public/index.html feature.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
+import { parse } from 'node-html-parser';
+
+// Update body content in public/index.html
+const root = parse( readFileSync( './index.html') );
+const publicRoot = parse( readFileSync( './public/index.html' ) );
+publicRoot.querySelector( 'body' ).innerHTML = root.querySelector( 'body' )
+  .childNodes.join( '' )
+  .toString()
+  .replaceAll( 'public/', '' );
+writeFileSync( './public/index.html', publicRoot.toString() );
 
 // https://vitejs.dev/config/
 export default defineConfig({
