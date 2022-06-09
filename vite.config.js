b6ec43690aa9bc4c02d5638c101f12285a87e757
Uwe Sölen Flaig <uwe.flaig@hella-gutmann.com>
2022-06-09 12:21:03 +0200

    Removed uncessary files, added text and audio.

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,45 @@ writeFileSync( './public/index.html', publicRoot.toString() );
 export default defineConfig({
   plugins: [
     VitePWA({
+      manifest: {
+        name: "simple-pwa",
+        short_name: "Simple PWA",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#00783e",
+        theme_color: "#00783e",
+        description: "A small example to show how to simple pwa.",
+        icons: [
+          {
+            src: "favicon/favicon_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any"
+          }, {
+            src: "favicon/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any"
+          }, {
+            src: "favicon/maskable_512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable"
+          }, {
+            src: "favicon/maskable.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "maskable"
+          }, {
+            src: "favicon/monochrome_256.png",
+            sizes: "256x256",
+            type: "image/png",
+            purpose: "monochrome"
+          }
+        ]
+      },
       workbox: {
-        globPatterns: [ '**/*.{js,css,html,svg,png}' ],
+        globPatterns: [ '**/*.{js,css,html,svg,png,mp3}' ],
       }
     }),
   ],
